1f05be10f6b0670d17835eae5fa64a4511ac1f19
abicorios <yaroslav.moroz91@gmail.com>
2019-08-15 23:35:48 +0300

    fix config

--- config.bat
+++ config.bat
@@ -2,3 +2,7 @@ if not exist caddy\Caddyfile (
 	echo localhost:8000 > caddy\Caddyfile
 	echo fastcgi / 127.0.0.1:9000 php >> caddy\Caddyfile
 )
+if not exist php.ini (
+copy php\php.ini-development php.ini
+echo extension_dir = %cd%\php\ext >> php.ini
+)
