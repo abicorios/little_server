2482a2ca1f954ed267c28804ee68750ad0e7b9cc
abicorios <yaroslav.moroz91@gmail.com>
2019-08-16 00:07:27 +0300

    mv config into install

--- install.bat
+++ install.bat
@@ -11,4 +11,13 @@ if exist install.ps1 powershell -ExecutionPolicy Bypass %cd%\install.ps1
 if exist install.ps1 del install.ps1
 if exist caddy.zip del caddy.zip
 if exist php.zip del php.zip
-config.bat
+if not exist caddy\Caddyfile (
+echo localhost:8000 > caddy\Caddyfile
+echo fastcgi / 127.0.0.1:9000 php >> caddy\Caddyfile
+mkdir public_html
+echo root %cd%\public_html >> caddy\Caddyfile
+)
+if not exist php.ini (
+copy php\php.ini-development php.ini
+echo extension_dir = %cd%\php\ext >> php.ini
+)
